bf1eac32ba66ad2e112c6d6294c625d3bb62cbe4
tomiconicgit <tomiconicx@icloud.com>
2025-10-08 15:24:00 +0100

    Delete src/objects/moon.js

--- src/objects/moon.js
+++
@@ -1,9 +0,0 @@
-// src/objects/moon.js: Creates large moon in sky
-
-export function createMoon() {
-    const geometry = new THREE.SphereGeometry(10, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    const moon = new THREE.Mesh(geometry, material);
-    moon.position.set(0, 20, -50);
-    return moon;
-}
