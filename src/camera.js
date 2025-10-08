c385ec2d86b4edbf0226367c98dddc0a76f2c12e
tomiconicgit <tomiconicx@icloud.com>
2025-10-08 16:50:19 +0100

    Delete src/camera.js

--- src/camera.js
+++
@@ -1,7 +0,0 @@
-// src/camera.js: Creates the camera
-
-export function createCamera() {
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 2, 0); // Position on/above terrain for first-person
-    return camera;
-}
