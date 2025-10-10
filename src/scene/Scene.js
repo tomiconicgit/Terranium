111df6e255fbe016fdffeb7f43cc30c85b4e5a67
tomiconicgit <tomiconicx@icloud.com>
2025-10-10 16:04:46 +0100

    Update Scene.js

--- src/scene/Scene.js
+++ src/scene/Scene.js
@@ -1,60 +1,46 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
 import { createGroundTiles } from '../objects/GroundTiles.js';
-import { createLaunchPad } from '../objects/LaunchPad.js';
 import { createProps } from '../objects/Props.js';
 import { createSkyDome } from '../objects/SkyDome.js';
+import { createLaunchPadComplex } from '../objects/LaunchPadComplex.js';
 
 export class Scene extends THREE.Scene {
-    constructor() {
-        super();
-        this.background = new THREE.Color(0x87a8c7);
-        this.userData.tick = [];
-        this.userData.uniforms = { time: { value: 0 } };
+  constructor() {
+    super();
+    this.background = new THREE.Color(0x87a8c7);
+    this.userData.uniforms = { time: { value: 0 } };
+    this.userData.tick = [];
 
-        // Ground (keeps name 'landscape' for raycasts)
-        const ground = createGroundTiles({
-            size: 100,
-            segments: 100,
-            grassRatio: 0.7,
-            uniformsRef: this.userData.uniforms
-        });
-        ground.name = 'landscape';
-        this.add(ground);
+    const ground = createGroundTiles({
+      size: 100, segments: 100, grassRatio: 0.9, uniformsRef: this.userData.uniforms
+    });
+    ground.name = 'landscape';
+    this.add(ground);
 
-        // Launch pad
-        const pad = createLaunchPad({ radius: 8, height: 0.2 });
-        pad.position.set(0, 0.01, 0);
-        this.add(pad);
+    // Massive NASA-style pad centered at origin
+    const pad = createLaunchPadComplex();
+    this.add(pad);
 
-        // Props
-        const { rocks, trees, tickers } = createProps({
-            areaSize: 100,
-            avoidRadius: 14,
-            rockCount: 180,
-            treeCount: 140,
-            uniformsRef: this.userData.uniforms
-        });
-        this.add(rocks, trees);
-        this.userData.tick.push(...tickers);
+    const { rocks, trees, tickers } = createProps({
+      areaSize: 100, avoidRadius: 22, rockCount: 160, treeCount: 150, uniformsRef: this.userData.uniforms
+    });
+    this.add(rocks, trees);
+    this.userData.tick.push(...tickers);
 
-        // Lighting
-        const hemi = new THREE.HemisphereLight(0xcfe8ff, 0x3a4a2a, 0.85);
-        hemi.position.set(0, 30, 0);
-        this.add(hemi);
+    const hemi = new THREE.HemisphereLight(0xcfe8ff, 0x3a4a2a, 0.9);
+    hemi.position.set(0, 30, 0);
+    this.add(hemi);
 
-        const sun = new THREE.DirectionalLight(0xffffff, 1.5);
-        sun.position.set(-40, 60, -25);
-        this.add(sun);
+    const sun = new THREE.DirectionalLight(0xffffff, 1.45);
+    sun.position.set(-40, 60, -25);
+    this.add(sun);
 
-        // Sky
-        const sky = createSkyDome();
-        this.add(sky);
-    }
+    const sky = createSkyDome();
+    this.add(sky);
+  }
 
-    update(dt, elapsed) {
-        // pump shared time uniform
-        this.userData.uniforms.time.value = elapsed;
-        // run material/animation tickers
-        for (const t of this.userData.tick) t(dt, elapsed);
-    }
+  update(dt, elapsed) {
+    this.userData.uniforms.time.value = elapsed;
+    for (const t of this.userData.tick) t(dt, elapsed);
+  }
 }
