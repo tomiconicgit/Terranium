cd1ac5c139b73d608a89ec26399226110abb8bb9
tomiconicgit <tomiconicx@icloud.com>
2025-10-10 22:31:37 +0100

    Update Scene.js

--- src/scene/Scene.js
+++ src/scene/Scene.js
@@ -1,41 +1,30 @@
-// Scene.js (only the key bits)
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
 import { createGroundTiles } from '../objects/GroundTiles.js';
-import { createSkyDome } from '../objects/SkyDome.js';
+import { createSkyDome }     from '../objects/SkyDome.js';
 import { createLaunchPadComplex } from '../objects/LaunchPadComplex.js';
 
 export class Scene extends THREE.Scene {
-  constructor() {
+  constructor(){
     super();
-
-    // Background must be null so our SkyDome shows.
-    this.background = null;
-
+    this.background = null;                       // allow skydome
     this.userData.uniforms = { time: { value: 0 } };
     this.userData.tick = [];
 
-    const ground = createGroundTiles({ size: 140, segments: 140, uniformsRef: this.userData.uniforms });
-    ground.name = 'landscape';
-    this.add(ground);
+    const ground = createGroundTiles({ size:140, segments:140, uniformsRef:this.userData.uniforms });
+    ground.name = 'landscape'; this.add(ground);
 
-    const pad = createLaunchPadComplex();
-    this.add(pad);
+    const pad = createLaunchPadComplex(); this.add(pad);
 
-    const hemi = new THREE.HemisphereLight(0xcfe8ff, 0x7c6a4d, 0.9);
-    hemi.position.set(0, 30, 0);
-    this.add(hemi);
-
-    const sun = new THREE.DirectionalLight(0xffffff, 1.4);
-    sun.position.set(-40, 60, -25);
-    this.add(sun);
+    const hemi = new THREE.HemisphereLight(0xcfe8ff, 0x7c6a4d, 0.9); hemi.position.set(0,30,0); this.add(hemi);
+    const sun  = new THREE.DirectionalLight(0xffffff, 1.35);        sun.position.set(-40,60,-25); this.add(sun);
 
     this.add(createSkyDome());
   }
 
-  update(dt, elapsed, camera, player) {
-    this.userData.uniforms.time.value = elapsed;
+  update(dt, t, camera, player){
+    this.userData.uniforms.time.value = t;
     const pad = this.getObjectByName('launchPad');
-    pad?.userData?.update?.(dt, elapsed, { camera, player });
-    for (const t of this.userData.tick) t(dt, elapsed, { camera, player });
+    pad?.userData?.update?.(dt, t, { camera, player });
+    for(const f of this.userData.tick) f(dt, t, { camera, player });
   }
 }
