22e8ff5b11ed52ade0fc6b8840e806040a4c6999
tomiconicgit <tomiconicx@icloud.com>
2025-10-10 17:00:57 +0100

    Update Scene.js

--- src/scene/Scene.js
+++ src/scene/Scene.js
@@ -1,6 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
-import { createGroundTiles } from '../objects/GroundTiles.js';
-import { createProps } from '../objects/Props.js';
+import { createGroundTiles } from '../objects/GroundTiles.js';   // now sand
 import { createSkyDome } from '../objects/SkyDome.js';
 import { createLaunchPadComplex } from '../objects/LaunchPadComplex.js';
 
@@ -11,9 +10,11 @@ export class Scene extends THREE.Scene {
     this.userData.uniforms = { time: { value: 0 } };
     this.userData.tick = [];
 
-    // Expanded terrain to 140×140
+    // 140×140 sandy environment
     const ground = createGroundTiles({
-      size: 140, segments: 140, grassRatio: 0.96, uniformsRef: this.userData.uniforms
+      size: 140,
+      segments: 140,
+      uniformsRef: this.userData.uniforms
     });
     ground.name = 'landscape';
     this.add(ground);
@@ -21,17 +22,11 @@ export class Scene extends THREE.Scene {
     const pad = createLaunchPadComplex();
     this.add(pad);
 
-    const { rocks, trees, tickers } = createProps({
-      areaSize: 140, avoidRadius: 30, rockCount: 180, treeCount: 170, uniformsRef: this.userData.uniforms
-    });
-    this.add(rocks, trees);
-    this.userData.tick.push(...tickers);
-
-    const hemi = new THREE.HemisphereLight(0xcfe8ff, 0x3a4a2a, 0.9);
+    const hemi = new THREE.HemisphereLight(0xcfe8ff, 0x7c6a4d, 0.8);
     hemi.position.set(0, 30, 0);
     this.add(hemi);
 
-    const sun = new THREE.DirectionalLight(0xffffff, 1.45);
+    const sun = new THREE.DirectionalLight(0xffffff, 1.35);
     sun.position.set(-40, 60, -25);
     this.add(sun);
 
@@ -41,6 +36,5 @@ export class Scene extends THREE.Scene {
 
   update(dt, elapsed) {
     this.userData.uniforms.time.value = elapsed;
-    for (const t of this.userData.tick) t(dt, elapsed);
   }
 }
