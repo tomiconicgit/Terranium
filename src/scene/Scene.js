3c7d50d08165fb55402ff0c173dd15c3574d9c19
tomiconicgit <tomiconicx@icloud.com>
2025-10-10 03:45:13 +0100

    Update Scene.js

--- src/scene/Scene.js
+++ src/scene/Scene.js
@@ -7,15 +7,20 @@ export class Scene extends THREE.Scene {
         super();
         this.background = new THREE.Color(0x000000);
         const landscape = createMoonLandscape();
-        landscape.name = 'landscape'; // Give the landscape a name to find it later
+        landscape.name = 'landscape'; 
         this.add(landscape);
         const sky = createSpaceSky();
         this.add(sky);
+
+        // --- Increase Light Intensity ---
         const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
         hemiLight.position.set(0, 20, 0);
+        hemiLight.intensity = 1.5; // Boost ambient light
         this.add(hemiLight);
+
         const dirLight = new THREE.DirectionalLight(0xffffff);
         dirLight.position.set(-3, 10, -10);
+        dirLight.intensity = 4.0; // Significantly boost the "sun"
         this.add(dirLight);
     }
 }
