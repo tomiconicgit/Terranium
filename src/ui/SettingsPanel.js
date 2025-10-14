7971ce2434f40c4e757e9d570dc52020d5710291
tomiconicgit <tomiconicx@icloud.com>
2025-10-14 18:28:52 +0100

    Update SettingsPanel.js

--- src/ui/SettingsPanel.js
+++ src/ui/SettingsPanel.js
@@ -5,9 +5,7 @@ export class SettingsPanel {
     this.panel = panelEl;
     this._changeCallback = null;
     
-    this.rotationY = 0; // Left/Right turn
-    this.rotationX = 0; // Up/Down tilt
-    this.rotationZ = 0; // Left/Right roll
+    this.rotationY = 0;
 
     // Get DOM elements
     this.primaryColorPicker = document.getElementById('primaryColor');
@@ -30,10 +28,6 @@ export class SettingsPanel {
     this.tessellationSlider = document.getElementById('tessellation');
     this.rotateLeftBtn = document.getElementById('rotate-left');
     this.rotateRightBtn = document.getElementById('rotate-right');
-    this.tiltLeftBtn = document.getElementById('tilt-left');
-    this.tiltRightBtn = document.getElementById('tilt-right');
-    this.rollLeftBtn = document.getElementById('roll-left');
-    this.rollRightBtn = document.getElementById('roll-right');
 
     this.isOpen = false;
     this.btn.addEventListener('click', () => this.toggle());
@@ -56,46 +50,14 @@ export class SettingsPanel {
       this.rotationY += Math.PI / 2;
       this.triggerChange();
     });
-
-    this.tiltLeftBtn.addEventListener('click', () => {
-      this.rotationX -= Math.PI / 2;
-      this.triggerChange();
-    });
-    this.tiltRightBtn.addEventListener('click', () => {
-      this.rotationX += Math.PI / 2;
-      this.triggerChange();
-    });
-    
-    this.rollLeftBtn.addEventListener('click', () => {
-      this.rotationZ -= Math.PI / 2;
-      this.triggerChange();
-    });
-    this.rollRightBtn.addEventListener('click', () => {
-      this.rotationZ += Math.PI / 2;
-      this.triggerChange();
-    });
     
     this.updateVisibility(); 
   }
 
-  onChange(callback) {
-    this._changeCallback = callback;
-  }
-
-  triggerChange() {
-    if (this._changeCallback) {
-      this._changeCallback(this.getSettings());
-    }
-  }
-
-  toggle() {
-    this.isOpen = !this.isOpen;
-    this.updateVisibility();
-  }
-
-  updateVisibility() {
-    this.panel.classList.toggle('hidden', !this.isOpen);
-  }
+  onChange(callback) { this._changeCallback = callback; }
+  triggerChange() { if (this._changeCallback) { this._changeCallback(this.getSettings()); } }
+  toggle() { this.isOpen = !this.isOpen; this.updateVisibility(); }
+  updateVisibility() { this.panel.classList.toggle('hidden', !this.isOpen); }
 
   getSettings() {
     return {
@@ -106,9 +68,8 @@ export class SettingsPanel {
       metalness: parseFloat(this.metalnessSlider.value),
       reflectivity: parseFloat(this.reflectivitySlider.value),
       rotationY: this.rotationY,
-      rotationX: this.rotationX,
-      rotationZ: this.rotationZ,
       tessellation: parseInt(this.tessellationSlider.value),
     };
   }
 }
+
