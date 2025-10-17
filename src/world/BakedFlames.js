d585fc1e0c3b625339e8400d0a6216e1c8698091
tomiconicgit <tomiconicx@icloud.com>
2025-10-17 23:01:17 +0100

    Create BakedFlames.js

---
+++ src/world/BakedFlames.js
@@ -0,0 +1,42 @@
+// src/world/BakedFlames.js
+// Master list of baked flame offsets used by InstancedFlames.
+// You can paste/append more entries here; indices are ignored at runtime.
+
+export const bakedFlameOffsets = [
+  // --- your earlier big set (0..22) ---
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 4.06, groupOffsetY: -3.0, groupOffsetZ:  5.31 },
+  { groupOffsetX: 6.49, groupOffsetY: -3.0, groupOffsetZ:  9.51 },
+  { groupOffsetX:10.06, groupOffsetY: -3.0, groupOffsetZ: 12.51 },
+  { groupOffsetX:14.23, groupOffsetY: -3.0, groupOffsetZ: 14.13 },
+  { groupOffsetX:18.76, groupOffsetY: -3.0, groupOffsetZ: 14.49 },
+  { groupOffsetX:23.17, groupOffsetY: -3.0, groupOffsetZ: 13.32 },
+  { groupOffsetX:26.98, groupOffsetY: -3.0, groupOffsetZ: 11.04 },
+  { groupOffsetX:29.89, groupOffsetY: -3.0, groupOffsetZ:  7.59 },
+
+  // (the repeated ones from your previous dump)
+  { groupOffsetX:15.25, groupOffsetY: -3.0, groupOffsetZ:  2.16 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ:  1.20 },
+
+  // --- your latest “add these to what we’ve already got” (8 more) ---
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+  { groupOffsetX: 3.10, groupOffsetY: -3.0, groupOffsetZ: 1.20 },
+];
