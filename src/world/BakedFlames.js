dc201db13ed3b69fa5e3154c27f39763a32a060f
tomiconicgit <tomiconicx@icloud.com>
2025-10-18 02:03:38 +0100

    Update BakedFlames.js

--- src/world/BakedFlames.js
+++ src/world/BakedFlames.js
@@ -3,7 +3,7 @@
 // Note: "index" is informational; InstancedFlames uses array order only.
 
 export const bakedFlameOffsets = [
-  // --- Existing 9 (forward/right arc) ---
+  // --- First 9 (forward/right arc) ---
   { groupOffsetX:  3.31, groupOffsetY: -3.0, groupOffsetZ:  1.23 },
   { groupOffsetX:  4.30, groupOffsetY: -3.0, groupOffsetZ:  5.58 },
   { groupOffsetX:  6.43, groupOffsetY: -3.0, groupOffsetZ:  9.48 },
@@ -14,9 +14,9 @@ export const bakedFlameOffsets = [
   { groupOffsetX: 26.77, groupOffsetY: -3.0, groupOffsetZ: 10.92 },
   { groupOffsetX: 29.62, groupOffsetY: -3.0, groupOffsetZ:  7.83 },
 
-  // --- New 11 (rear/left arc) ---
-  { groupOffsetX:  3.58, groupOffsetY: -3.0, groupOffsetZ: -3.69 },
-  { groupOffsetX:  5.53, groupOffsetY: -3.0, groupOffsetZ: -7.71 },
+  // --- Next 11 (rear/left arc) ---
+  { groupOffsetX:  3.58, groupOffsetY: -3.0, groupOffsetZ:  -3.69 },
+  { groupOffsetX:  5.53, groupOffsetY: -3.0, groupOffsetZ:  -7.71 },
   { groupOffsetX:  8.32, groupOffsetY: -3.0, groupOffsetZ: -11.07 },
   { groupOffsetX: 12.10, groupOffsetY: -3.0, groupOffsetZ: -13.47 },
   { groupOffsetX: 16.69, groupOffsetY: -3.0, groupOffsetZ: -14.19 },
@@ -25,5 +25,17 @@ export const bakedFlameOffsets = [
   { groupOffsetX: 29.11, groupOffsetY: -3.0, groupOffsetZ:  -9.30 },
   { groupOffsetX: 31.12, groupOffsetY: -3.0, groupOffsetZ:  -5.55 },
   { groupOffsetX: 32.02, groupOffsetY: -3.0, groupOffsetZ:  -1.29 },
-  { groupOffsetX: 31.78, groupOffsetY: -3.0, groupOffsetZ:   3.51 }
+  { groupOffsetX: 31.78, groupOffsetY: -3.0, groupOffsetZ:   3.51 },
+
+  // --- Final 10 (center / inner ring) ---
+  { groupOffsetX:  9.82, groupOffsetY: -3.0, groupOffsetZ:  0.75 },
+  { groupOffsetX: 12.13, groupOffsetY: -3.0, groupOffsetZ:  5.52 },
+  { groupOffsetX: 15.40, groupOffsetY: -3.0, groupOffsetZ:  2.07 },
+  { groupOffsetX: 20.71, groupOffsetY: -3.0, groupOffsetZ:  0.99 },
+  { groupOffsetX: 17.20, groupOffsetY: -3.0, groupOffsetZ:  7.95 },
+  { groupOffsetX: 22.48, groupOffsetY: -3.0, groupOffsetZ:  6.69 },
+  { groupOffsetX: 25.69, groupOffsetY: -3.0, groupOffsetZ:  2.43 },
+  { groupOffsetX: 25.15, groupOffsetY: -3.0, groupOffsetZ: -3.36 },
+  { groupOffsetX: 21.22, groupOffsetY: -3.0, groupOffsetZ: -7.29 },
+  { groupOffsetX: 15.64, groupOffsetY: -3.0, groupOffsetZ: -7.89 }
 ];
