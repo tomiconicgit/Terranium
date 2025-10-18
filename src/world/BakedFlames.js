7d5b29ac1d4c40cde775f7cd50071e303cd829c2
tomiconicgit <tomiconicx@icloud.com>
2025-10-18 01:56:29 +0100

    Update BakedFlames.js

--- src/world/BakedFlames.js
+++ src/world/BakedFlames.js
@@ -1,8 +1,9 @@
 // src/world/BakedFlames.js
 // Master list of baked flame offsets used by InstancedFlames.
-// Note: "index" is informational; InstancedFlames uses array order.
+// Note: "index" is informational; InstancedFlames uses array order only.
 
 export const bakedFlameOffsets = [
+  // --- Existing 9 (forward/right arc) ---
   { groupOffsetX:  3.31, groupOffsetY: -3.0, groupOffsetZ:  1.23 },
   { groupOffsetX:  4.30, groupOffsetY: -3.0, groupOffsetZ:  5.58 },
   { groupOffsetX:  6.43, groupOffsetY: -3.0, groupOffsetZ:  9.48 },
@@ -11,5 +12,18 @@ export const bakedFlameOffsets = [
   { groupOffsetX: 18.58, groupOffsetY: -3.0, groupOffsetZ: 14.31 },
   { groupOffsetX: 22.84, groupOffsetY: -3.0, groupOffsetZ: 13.35 },
   { groupOffsetX: 26.77, groupOffsetY: -3.0, groupOffsetZ: 10.92 },
-  { groupOffsetX: 29.62, groupOffsetY: -3.0, groupOffsetZ:  7.83 }
+  { groupOffsetX: 29.62, groupOffsetY: -3.0, groupOffsetZ:  7.83 },
+
+  // --- New 11 (rear/left arc) ---
+  { groupOffsetX:  3.58, groupOffsetY: -3.0, groupOffsetZ: -3.69 },
+  { groupOffsetX:  5.53, groupOffsetY: -3.0, groupOffsetZ: -7.71 },
+  { groupOffsetX:  8.32, groupOffsetY: -3.0, groupOffsetZ: -11.07 },
+  { groupOffsetX: 12.10, groupOffsetY: -3.0, groupOffsetZ: -13.47 },
+  { groupOffsetX: 16.69, groupOffsetY: -3.0, groupOffsetZ: -14.19 },
+  { groupOffsetX: 21.19, groupOffsetY: -3.0, groupOffsetZ: -13.83 },
+  { groupOffsetX: 25.36, groupOffsetY: -3.0, groupOffsetZ: -12.36 },
+  { groupOffsetX: 29.11, groupOffsetY: -3.0, groupOffsetZ:  -9.30 },
+  { groupOffsetX: 31.12, groupOffsetY: -3.0, groupOffsetZ:  -5.55 },
+  { groupOffsetX: 32.02, groupOffsetY: -3.0, groupOffsetZ:  -1.29 },
+  { groupOffsetX: 31.78, groupOffsetY: -3.0, groupOffsetZ:   3.51 }
 ];
